a1c2664b02ec50b010d7aad4037d18f5c659aca9
rogetsun <songyw163@163.com>
2015-11-23 15:46:33 +0800

    jquery.websocket

---
+++ web/assets/jquery/jquery-websocket.js
@@ -0,0 +1,42 @@
+/**
+ * Created by uv2sun on 15/11/23.
+ */
+;(function ($) {
+    function WS(url, callbacks) {
+        var host = window.location.host;
+        var path = window.location.pathname;
+        var webp = path.substring(0, path.substring(1).indexOf("/") + 1);
+        this.websocket = new WebSocket("ws://" + host + webp + "/ws" + url);
+        var _this = this;
+        if (callbacks) {
+            if (callbacks.onmessage) {
+                this.websocket.onmessage = function (event) {
+                    callbacks.onmessage(event, _this.websocket);
+                }
+            }
+            if (callbacks.onopen) {
+                this.websocket.onopen = function (event) {
+                    callbacks.onopen(event, _this.websocket);
+                }
+            }
+            if (callbacks.onerror) {
+                this.websocket.onerror = function (event) {
+                    callbacks.onerror(event, _this.websocket);
+                }
+            }
+            if (callbacks.onclose) {
+                this.websocket.onclose = function (event) {
+                    callbacks.onclose(event, _this.websocket);
+                }
+            }
+        }
+    }
+
+    $.extend({
+        websocket: function (url, callbacks) {
+            return new WS(url, callbacks);
+        }
+    });
+
+
+})(jQuery);
