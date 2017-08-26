20d58de057547b178a373b6966ad9bcd5e08255d
rogetsun <songyw163@163.com>
2017-08-27 00:08:05 +0800

    修改@ReceiveMsgType注解为可以继承到子类的

--- src/com/uv/websocket/annotation/ReceiveMsgType.java
+++ src/com/uv/websocket/annotation/ReceiveMsgType.java
@@ -1,15 +1,13 @@
 package com.uv.websocket.annotation;
 
-import java.lang.annotation.ElementType;
-import java.lang.annotation.Retention;
-import java.lang.annotation.RetentionPolicy;
-import java.lang.annotation.Target;
+import java.lang.annotation.*;
 
 /**
  * Created by uv2sun on 15/11/21.
  */
 @Target(ElementType.METHOD)
 @Retention(RetentionPolicy.RUNTIME)
+@Inherited
 public @interface ReceiveMsgType {
     String value();
 }
